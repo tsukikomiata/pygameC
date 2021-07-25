c280dc9d92cf9d9d38de2654e2010d07893b4527
tsukikomiata <85107866+tsukikomiata@users.noreply.github.com>
2021-07-25 19:06:47 +1000

    Everything

--- main.py
+++ main.py
@@ -19,31 +19,23 @@ def cancel():
 
 while running:
     for event in pg.event.get():
+
         if event.type == pg.QUIT:
             running = False
-
         if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
             running = False
-
         if event.type == pg.MOUSEBUTTONDOWN:
             drawing = True
             x0, y0 = event.pos
-
         if event.type == pg.MOUSEBUTTONUP:
             arr.append((x0, y0, x1, y1))
             drawing = False
-
         if event.type == pg.MOUSEMOTION:
             x1, y1 = event.pos[0] - x0, event.pos[1] - y0
-
-        # if event.type == pg.KEYDOWN and event.mod == pg.KMOD_LCTRL and event.key == pg.K_z:
-        #     cancel()
-
         if event.type == pg.KEYDOWN:
             keys = pg.key.get_pressed()
-
-        if any((keys[pg.K_LCTRL], keys[pg.K_RCTRL])) and keys[pg.K_z]:
-            cancel()
+            if any((keys[pg.K_LCTRL], keys[pg.K_RCTRL])) and keys[pg.K_z]:
+                cancel()
 
     screen.fill(pg.Color('black'))
     if drawing:
