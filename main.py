02edd40e2f01126b49b0e7873e3067f749b87374
tsukikomiata <85107866+tsukikomiata@users.noreply.github.com>
2021-07-25 19:00:18 +1000

    try

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import pygame as pg
 pg.init()
 clock = pg.time.Clock()
 FPS = 10
+x0, y0, x1, y1 = (0, 0, 0, 0)
 size = (500, 500)
-x, y, w, h = 0, 0, 0, 0
 screen = pg.display.set_mode(size)
+# pg.display.flip()
 arr = []
 running = True
 drawing = False
@@ -20,21 +21,33 @@ while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+
         if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
             running = False
+
         if event.type == pg.MOUSEBUTTONDOWN:
             drawing = True
-            x, y = event.pos
+            x0, y0 = event.pos
+
         if event.type == pg.MOUSEBUTTONUP:
-            arr.append((x, y, w, h))
+            arr.append((x0, y0, x1, y1))
             drawing = False
+
         if event.type == pg.MOUSEMOTION:
-            w, h = event.pos[0] - x, event.pos[1] - y
-        if event.type == pg.KEYDOWN and event.mod == pg.KMOD_LCTRL and event.key == pg.K_z:
+            x1, y1 = event.pos[0] - x0, event.pos[1] - y0
+
+        # if event.type == pg.KEYDOWN and event.mod == pg.KMOD_LCTRL and event.key == pg.K_z:
+        #     cancel()
+
+        if event.type == pg.KEYDOWN:
+            keys = pg.key.get_pressed()
+
+        if any((keys[pg.K_LCTRL], keys[pg.K_RCTRL])) and keys[pg.K_z]:
             cancel()
+
     screen.fill(pg.Color('black'))
     if drawing:
-        pg.draw.rect(screen, (0, 255, 127), ((x, y), (w, h)), 1)
+        pg.draw.rect(screen, (0, 255, 127), ((x0, y0), (x1, y1)), 1)
     for coords in arr:
         pg.draw.rect(screen, (0, 255, 127), coords, 1)
     pg.display.flip()
